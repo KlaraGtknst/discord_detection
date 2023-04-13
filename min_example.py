4e890997e8084d5248b1aa54449c44f9cd16725c
KlaraGtknst <klara.gutekunst.kg@gmail.com>
2023-04-13 19:47:25 +0200

    distance between two windows calculated using squared euclidean distance

--- min_example.py
+++ min_example.py
@@ -23,18 +23,21 @@ if __name__ == "__main__":
     hot_sax = HOTSAX(sax=sax)
 
     # example
-    dat = np.array([-2, 0, 2, 0, -1])
+    dat = np.array([-2, 0, 2, 0, -1, 3, 0, 0, 2, -1, 1, 2])
     sax.fit(X=dat)
 
     #symbols = sax.transform(X=dat, return_as='idx')
     #print(3, symbols)
     Y = hot_sax.compare_pairwise(window_size=2, X=dat)
 
-    print(Y)
+    print('solution:', Y)
 
 
 
 if __name__ == "__main__":
     # TODO: get array to matrix  it window size einträgen im inneren, so das die verleichbar
     dat = np.array([-2, 0, 2, 0, -1])
-    print(np.expand_dims(dat, axis=(3,2)))
+    # 3 = num of splits
+    dat = np.array_split(dat, 3)
+    #print(np.expand_dims(dat, axis=(3,2)))
+    #print(dat)
--- src/HOT_SAX.py
+++ src/HOT_SAX.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.stats import norm
 from functools import reduce
 from scipy.spatial.distance import pdist
+from itertools import combinations
 
 __author__ = "Klara Gutekunst"
 __copyright__ = "Copyright 2023, Intelligent Embedded Systems, Universität Kassel/Finanzamt Kassel"
@@ -29,9 +30,20 @@ class HOTSAX:
         #np.apply_along_axis(lambda x,y: chr(x[0] + ord('a')), 0, np.expand_dims(S, axis=0))
         #print(1, np.expand_dims(X, axis=1))
         # compare paa version (only one value for paa_length many entries
-        if window_size != 1:
-            print([X[i:i+window_size] for i in range(0, len(X), window_size)])
-            Y = [sum(pdist(X=np.expand_dims(X[i:i+window_size], axis=1), metric='sqeuclidean')) for i in range(0, len(X), window_size)]
+
+        print(window_size, X)
+        if (window_size != 1) and (window_size < len(X)):
+            # windows entries contain data points, which belong to the same window
+            windows = np.array_split(X, len(X)//window_size)
+            print('windows:', windows)
+
+            # sum up die squared euclidean distance per respective entry of two windows
+            combis = list(combinations(np.arange(0, len(windows), 1), 2))
+            print('Combis', combis)
+            Y = [[sum((x[0] - x[1]) ** 2 for x in zip(windows[window1], windows[window2]))] for window1, window2 in combis]
+
+            #print([X[i:i+window_size] for i in range(0, len(X), window_size)])
+            #Y = [sum(pdist(X=np.expand_dims(X[i:i+window_size], axis=1), metric='sqeuclidean')) for i in range(0, len(X), window_size)]
         else:
             Y = pdist(X=np.expand_dims(X, axis=1), metric='sqeuclidean')
         return Y
