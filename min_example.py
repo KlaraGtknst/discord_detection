f7be12bfc7f38eed45e1d7f60a080e4ff8011370
KlaraGtknst <klara.gutekunst.kg@gmail.com>
2023-04-13 20:05:01 +0200

    code clean up 4

--- min_example.py
+++ min_example.py
@@ -3,41 +3,41 @@ from src.HOT_SAX import HOTSAX
 import numpy as np
 
 if __name__ == "__main__":
-    # Instantiate the class
-    sax = SAX(symbols=4, paa_length=3)
-    #print(sax.paa_length)
 
-    # example
-    dat = np.array([-2, 0, 2, 0, -1])
+    # functions to test code
+    def try_SAX():
+        # Instantiate the class
+        sax = SAX(symbols=4, paa_length=3)
 
-    sax.fit(X=dat)
-    symbols = sax.transform(X=dat, return_as='symbol')
-    #print(symbols)
+        # example
+        dat = np.array([-2, 0, 2, 0, -1])
+        sax.fit(X=dat)
+        symbols = sax.transform(X=dat, return_as='symbol')
 
-    #print(sax.reconstruct(S=symbols, reconstruct_from='symbol'))
+        print('symbols: ', symbols)
+        print('reconstructed values from symbols: ', sax.reconstruct(S=symbols, reconstruct_from='symbol'))
+
+        symbols = sax.transform(X=dat, return_as='idx')
+        print('integers: ', symbols)
+
+    def try_HOT_SAX():
+        # Instantiate the class
+        sax = SAX(symbols=4, paa_length=3)
+        hot_sax = HOTSAX(sax=sax)
+
+        # example
+        dat = np.array([-2, 0, 2, 0, -1, 3, 0, 0, 2, -1, 1, 2])
+        sax.fit(X=dat)
+        Y = hot_sax.compare_pairwise(window_size=2, X=dat)
+
+        print('solution with window size 2:', Y)
+
+        Y = hot_sax.compare_pairwise(window_size=1, X=dat)
+
+        print('solution with window size 1:', Y)
 
 
-if __name__ == "__main__":
-    # Instantiate the class
-    sax = SAX(symbols=4, paa_length=3)
-    hot_sax = HOTSAX(sax=sax)
+    # test code (calling functions)
+    try_SAX()
+    try_HOT_SAX()
 
-    # example
-    dat = np.array([-2, 0, 2, 0, -1, 3, 0, 0, 2, -1, 1, 2])
-    sax.fit(X=dat)
-
-    #symbols = sax.transform(X=dat, return_as='idx')
-    #print(3, symbols)
-    Y = hot_sax.compare_pairwise(window_size=2, X=dat)
-
-    print('solution:', Y)
-
-
-
-if __name__ == "__main__":
-    # TODO: get array to matrix  it window size einträgen im inneren, so das die verleichbar
-    dat = np.array([-2, 0, 2, 0, -1])
-    # 3 = num of splits
-    dat = np.array_split(dat, 3)
-    #print(np.expand_dims(dat, axis=(3,2)))
-    #print(dat)
